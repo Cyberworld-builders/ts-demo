2ea79d01a7d7689a8540afbec9fbf09835419a47
Jay <jaylong255@gmail.com>
2025-05-13 21:58:27 +0000

    adding promises

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -3,7 +3,7 @@ import { developmentConfig, testConfig } from './database';
 
 let connection: Connection | null = null;
 
-export const initializeDatabase = async (isTest = false) => {
+export const initializeDatabase = async (isTest = false): Promise<Connection> => {
   if (connection) {
     return connection;
   }
@@ -13,7 +13,7 @@ export const initializeDatabase = async (isTest = false) => {
   return connection;
 };
 
-export const closeConnection = async () => {
+export const closeConnection = async (): Promise<void> => {
   if (connection) {
     await connection.close();
     connection = null;
