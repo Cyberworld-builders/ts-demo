286f13c47bca25c942bed59b665e0953fc48066d
Jay <jaylong255@gmail.com>
2025-05-13 22:04:54 +0000

    telling the linter to ignore jest config

--- eslint.config.js
+++ eslint.config.js
@@ -2,43 +2,46 @@ import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 
-export default [
-  eslint.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
+export default {
+  ignores: ['jest.config.js'],
+  configs: [
+    eslint.configs.recommended,
+    {
+      files: ['**/*.ts'],
+      languageOptions: {
+        parser: tsparser,
+        parserOptions: {
+          ecmaVersion: 2020,
+          sourceType: 'module',
+        },
+        globals: {
+          // Jest globals
+          jest: 'readonly',
+          describe: 'readonly',
+          it: 'readonly',
+          expect: 'readonly',
+          beforeEach: 'readonly',
+          afterEach: 'readonly',
+          beforeAll: 'readonly',
+          afterAll: 'readonly',
+          // Node globals
+          console: 'readonly',
+          module: 'readonly'
+        }
       },
-      globals: {
-        // Jest globals
-        jest: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        // Node globals
-        console: 'readonly',
-        module: 'readonly'
-      }
+      plugins: {
+        '@typescript-eslint': tseslint,
+      },
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': ['error', { 
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }],
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+      },
     },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': ['error', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_'
-      }],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-    },
-  },
-]; 
+  ]
+}; 
