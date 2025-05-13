c9123097abf8ee96536094076e85170c0ce18b1c
Jay <jaylong255@gmail.com>
2025-05-13 22:07:41 +0000

    fixing lint config for v9

--- eslint.config.js
+++ eslint.config.js
@@ -2,46 +2,46 @@ import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 
-export default {
-  ignores: ['jest.config.js'],
-  configs: [
-    eslint.configs.recommended,
-    {
-      files: ['**/*.ts'],
-      languageOptions: {
-        parser: tsparser,
-        parserOptions: {
-          ecmaVersion: 2020,
-          sourceType: 'module',
-        },
-        globals: {
-          // Jest globals
-          jest: 'readonly',
-          describe: 'readonly',
-          it: 'readonly',
-          expect: 'readonly',
-          beforeEach: 'readonly',
-          afterEach: 'readonly',
-          beforeAll: 'readonly',
-          afterAll: 'readonly',
-          // Node globals
-          console: 'readonly',
-          module: 'readonly'
-        }
+export default [
+  {
+    ignores: ['jest.config.js'],
+  },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
       },
-      plugins: {
-        '@typescript-eslint': tseslint,
-      },
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'warn',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/no-unused-vars': ['error', { 
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }],
-        'no-console': ['warn', { allow: ['warn', 'error'] }],
-      },
+      globals: {
+        // Jest globals
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        // Node globals
+        console: 'readonly',
+        module: 'readonly'
+      }
     },
-  ]
-}; 
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+]; 
